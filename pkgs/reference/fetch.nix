422ea9abd0450d1badf24f1275e97620ed1f9d5d
molaco <hugo.cas2@icloud.com>
2025-09-13 13:32:39 +0200

    fix redability versions issues?

--- pkgs/reference/fetch.nix
+++ pkgs/reference/fetch.nix
@@ -1,10 +1,10 @@
-{ callPackage, python3Packages }:
+{ callPackage, python314Packages }:
 callPackage ./generic-python.nix {
   service = "fetch";
   # relaxing version constraints until the following issue is closed.
   # https://github.com/modelcontextprotocol/servers/issues/1146
   pythonRelaxDeps = [ "httpx" ];
-  dependencies = with python3Packages; [
+  dependencies = with python314Packages; [
     markdownify
     mcp
     protego
